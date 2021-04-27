59257ff60c732618da862ee171cdbb131de225de
Kathy Jung <kjung0909@KJs-MacBook-Air.local>
2021-04-27 01:19:52 -0700

    update 4/27/21

--- table scripts/pregnancy-immune tables.R
+++ table scripts/pregnancy-immune tables.R
@@ -60,12 +60,12 @@ full_adj_res <- rbind(H1adj, H2adj, H3adj, H4adj)
 
 #### Table 2 ####
 exposure <- c("vitD_nmol_per_L", "logFERR_inf", "logSTFR_inf", "logRBP_inf", "vit_D_def", "vit_A_def", "iron_def") 
-outcome <- c("t2_ln_agp", "t2_ln_crp", 
-             "t2_ln_ifn", "sumscore_t2_Z", 
-             "t3_ln_ifn","sumscore_t3_Z")
+outcome <- c("t2_ln_crp", "t2_ln_agp", "t2_ln_ifn", "sumscore_t2_Z", 
+             "t3_ln_crp", "t3_ln_agp", "t3_ln_ifn","sumscore_t3_Z")
 expo_var <- c("Vit D (nmol/L)", "Log ferritin", "Log sTfR", "Log RBP", "Vit D deficiency", "Vit A deficiency", "Iron deficiency") 
-out_var <- c("Ln AGP Age 14 months", "Ln CRP Age 14 months", 
+out_var <- c("Ln CRP Age 14 months", "Ln AGP Age 14 months", 
              "Ln IFN Age 14 months", "Sum score of 13 cytokines Age 14 months", 
+             "Ln CRP Age 28 months", "Ln AGP Age 28 months", 
              "Ln IFN Age 28 months", "Sum score of 13 cytokines Age 28 months")
 
 tbl2 <- growth_tbl("Maternal Nutrition and Child Inflammation", expo_var, out_var, exposure, outcome, H1, H1adj, T)
@@ -74,13 +74,13 @@ tbl2supp <- growth_tbl("Maternal Nutrition and Child Inflammation", expo_var, ou
 tbl2flexsupp <- growth_tbl_flex("Maternal Nutrition and Child Inflammation", expo_var, out_var, exposure, outcome, H1, H1adj,)
 
 #### Table 3 ####
-exposure <- c("preg_cort") 
-outcome <- c("t2_ln_agp", "t2_ln_crp", 
-             "t2_ln_ifn", "sumscore_t2_Z", 
-             "t3_ln_ifn","sumscore_t3_Z")
-expo_var <- c("Pregnancy Cortisol") 
-out_var <- c("Ln AGP Age 14 months", "Ln CRP Age 14 months", 
+exposure <- c("ln_preg_cort") 
+outcome <- c("t2_ln_crp", "t2_ln_agp", "t2_ln_ifn", "sumscore_t2_Z", 
+             "t3_ln_crp", "t3_ln_agp", "t3_ln_ifn","sumscore_t3_Z")
+expo_var <- c("Ln Pregnancy Cortisol") 
+out_var <- c("Ln CRP Age 14 months", "Ln AGP Age 14 months", 
              "Ln IFN Age 14 months", "Sum score of 13 cytokines Age 14 months", 
+             "Ln CRP Age 28 months", "Ln AGP Age 28 months", 
              "Ln IFN Age 28 months", "Sum score of 13 cytokines Age 28 months")
 
 tbl3 <- growth_tbl("Maternal Stress and Child Inflammation", expo_var, out_var, exposure, outcome, H2, H2adj, T)
@@ -91,13 +91,13 @@ tbl3flexsupp <- growth_tbl_flex("Maternal Stress and Child Inflammation", expo_v
 
 #### Table 4 ####
 
-exposure <- c("logCRP", "mom_t0_ln_ifn", "sumscore_t0_mom_Z")   
-outcome <- c("t2_ln_agp", "t2_ln_crp", 
-             "t2_ln_ifn", "sumscore_t2_Z", 
-             "t3_ln_ifn","sumscore_t3_Z")
-expo_var <- c("Log CRP", "Maternal Initial Ln IFN", "Maternal sum score of 13 cytokines") 
-out_var <- c("Ln AGP Age 14 months", "Ln CRP Age 14 months", 
+exposure <- c("logCRP", "log AGP", "mom_t0_ln_ifn", "sumscore_t0_mom_Z")   
+outcome <- c("t2_ln_crp", "t2_ln_agp", "t2_ln_ifn", "sumscore_t2_Z", 
+             "t3_ln_crp", "t3_ln_agp", "t3_ln_ifn","sumscore_t3_Z")
+expo_var <- c("Log CRP", "Log AGP", "Maternal Initial Ln IFN", "Maternal sum score of 13 cytokines") 
+out_var <- c("Ln CRP Age 14 months", "Ln AGP Age 14 months", 
              "Ln IFN Age 14 months", "Sum score of 13 cytokines Age 14 months", 
+             "Ln CRP Age 28 months", "Ln AGP Age 28 months", 
              "Ln IFN Age 28 months", "Sum score of 13 cytokines Age 28 months")
 
 tbl4 <- growth_tbl("Maternal Inflammation and Child Inflammation", expo_var, out_var, exposure, outcome, H3, H3adj, T)
@@ -107,14 +107,14 @@ tbl4flexsupp <- growth_tbl_flex("Maternal Inflammation and Child Inflammation", 
 
 #### Table 5 ####
 
-exposure <- c("preg_estri") 
-outcome <- c("t2_ln_agp", "t2_ln_crp", 
-             "t2_ln_ifn", "sumscore_t2_Z", 
-             "t3_ln_ifn","sumscore_t3_Z")
-expo_var <- c("Pregnancy Estriol") 
-out_var <- c("Ln AGP at age 14 months", "Ln CRP at age 14 months", 
-             "Ln IFN at age 14 months", "Sum score of 13 cytokines at age 14 months", 
-             "Ln IFN at age 28 months", "Sum score of 13 cytokines at age 28 months")
+exposure <- c("ln_preg_estri") 
+outcome <- c("t2_ln_crp", "t2_ln_agp", "t2_ln_ifn", "sumscore_t2_Z", 
+             "t3_ln_crp", "t3_ln_agp", "t3_ln_ifn","sumscore_t3_Z")
+expo_var <- c("Ln Pregnancy Estriol") 
+out_var <- c("Ln CRP Age 14 months", "Ln AGP Age 14 months", 
+             "Ln IFN Age 14 months", "Sum score of 13 cytokines Age 14 months", 
+             "Ln CRP Age 28 months", "Ln AGP Age 28 months", 
+             "Ln IFN Age 28 months", "Sum score of 13 cytokines Age 28 months")
 
 tbl5 <- growth_tbl("Maternal Stress and Child Inflammation", expo_var, out_var, exposure, outcome, H4, H4adj, T)
 tbl5flex <- growth_tbl_flex("Maternal Estriol and Child Inflammation", expo_var, out_var, exposure, outcome, H4, H4adj, T)
